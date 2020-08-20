af048dbc8a320ee7eae1c1d056f660041bc790c6
TelmanYusifov <43857129+TelmanYusifov@users.noreply.github.com>
2020-08-20 09:49:26 +0400

    About and Contact deleted.

--- Hospital/Controllers/HomeController.cs
+++ Hospital/Controllers/HomeController.cs
@@ -12,50 +12,25 @@ namespace Hospital.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Doctors()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
         public ActionResult Patients()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         public ActionResult Appointments()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         public ActionResult Schedule()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
         public ActionResult Departments()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
